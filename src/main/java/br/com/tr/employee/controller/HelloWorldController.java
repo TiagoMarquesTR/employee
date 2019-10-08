7a78d7d580a835be0a716616c81e4fbf8e859d8e
Tiago Marques <tiago.marques@thomsonreuters.com>
2019-10-08 15:00:41 -0300

    First commit to create the project

---
+++ src/main/java/br/com/tr/employee/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package br.com.tr.employee.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "Hello World";
+    }
+}
