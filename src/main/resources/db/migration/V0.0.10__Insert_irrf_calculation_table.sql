53f3199630db8ffe3ed72897aaca0b1f4424fb0e
FilipePereiraTR <filipe.pereira@thomsonreuters.com>
2019-10-09 21:10:47 +0100

    Initial data insert

---
+++ src/main/resources/db/migration/V0.0.10__Insert_irrf_calculation_table.sql
@@ -0,0 +1,5 @@
+INSERT INTO irrf_calculation_table VALUES ('2019-01-01', 1903.98, 0.00, 0.00);
+INSERT INTO irrf_calculation_table VALUES ('2019-01-01', 2826.65, 7.50, 142.80);
+INSERT INTO irrf_calculation_table VALUES ('2019-01-01', 3751.05, 15.00, 354.80);
+INSERT INTO irrf_calculation_table VALUES ('2019-01-01', 4664.68, 22.50, 636.13);
+INSERT INTO irrf_calculation_table VALUES ('2019-01-01', 9999999999.99, 27.50, 869.36);
